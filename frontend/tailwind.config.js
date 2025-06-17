2a5530b6a56075791ed238de5d14b469482210e8
Kethineni venkata subbaiah chowdary <129073951+Subbu-chowdary@users.noreply.github.com>
2025-06-17 10:34:22 +0530

    Update tailwind.config.js for render

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,37 @@
+// module.exports = {
+//    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+//    darkMode: false, // or 'media' or 'class'
+//    theme: {
+//       extend: {
+//          boxShadow: {
+//             card: '14px 14px 28px rgba(144,166,186,1),-14px -14px 28px rgba(230,255,255,0.5)',
+//          },
+//          backgroundColor: {
+//             winner: '#bbd8f2',
+//          },
+//       },
+//    },
+//    variants: {
+//       extend: {},
+//    },
+//    plugins: [],
+// };
+
+
 module.exports = {
-   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-   darkMode: false, // or 'media' or 'class'
-   theme: {
-      extend: {
-         boxShadow: {
-            card: '14px 14px 28px rgba(144,166,186,1),-14px -14px 28px rgba(230,255,255,0.5)',
-         },
-         backgroundColor: {
-            winner: '#bbd8f2',
-         },
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Replaced 'purge' with 'content'
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      boxShadow: {
+        card: '14px 14px 28px rgba(144,166,186,1),-14px -14px 28px rgba(230,255,255,0.5)',
       },
-   },
-   variants: {
-      extend: {},
-   },
-   plugins: [],
+      backgroundColor: {
+        winner: '#bbd8f2',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'), // Added forms plugin
+  ],
 };
