adf8852ffdea9e95bb07031babc8fa9af736afca
Kethineni venkata subbaiah chowdary <129073951+Subbu-chowdary@users.noreply.github.com>
2025-06-18 10:49:42 +0530

    Update app.js allowed all cors

--- backend/app.js
+++ backend/app.js
@@ -6,19 +6,29 @@ const path = require('path');
 
 const errorMiddleware = require('./middlewares/error');
 
+// app.use(
+//    cors({
+//       origin: ['http://localhost:3000', 'http://127.0.0.1:3000','https://voting-frontend.netlify.app'],
+//       credentials: true,
+//    })
+// );
+// app.use(function (req, res, next) {
+//    res.header('Access-Control-Allow-Origin', 'http://localhost:3000','https://voting-frontend.netlify.app');
+//    res.header('Access-Control-Allow-Headers', true);
+//    res.header('Access-Control-Allow-Credentials', true);
+//    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//    next();
+// });
 app.use(
-   cors({
-      origin: ['http://localhost:3000', 'http://127.0.0.1:3000','https://voting-frontend.netlify.app'],
-      credentials: true,
-   })
+  cors({
+    origin: (origin, callback) => {
+      callback(null, origin); // allow all origins dynamically
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
 );
-app.use(function (req, res, next) {
-   res.header('Access-Control-Allow-Origin', 'http://localhost:3000','https://voting-frontend.netlify.app');
-   res.header('Access-Control-Allow-Headers', true);
-   res.header('Access-Control-Allow-Credentials', true);
-   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-   next();
-});
 app.use(express.json()); //instead of body-parser
 app.use(cookieParser());
 console.log(process.env.NODE_ENV);
